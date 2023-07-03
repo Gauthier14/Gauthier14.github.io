689f4bb3b2d0ef8c0d2255a7ff8c6fd832c7f714
Gauthier14 <lobeaugauthier@gmail.com>
2023-07-03 10:44:03 +0200

    yt video

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gauthier14.github.io',
+    integrations: [astroImageTools],
 });
+
